0a8fefe1c19223f1d7e9b71205472f714d1a6534
Dian Yuanda <yuanda.dian@gmail.com>
2018-06-11 14:14:14 +0700

    Add Passport JWT Authentication Strategy to Login

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,7 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 // Load Users Model
 const User = require('../../models/User');
@@ -81,6 +82,17 @@ router.post('/login', (req, res) => {
         return res.status(400).json({password: 'Password or Email Doesnt Match'});
       }
     });
+  });
+});
+
+// @route   GET api/users/current
+// @desc    Return Current User
+// @access  Private
+router.get('/current', passport.authenticate('jwt', {session:false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
   });
 });
 
