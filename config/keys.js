3e3b095527622e5760e52d06905f926673eb9a46
Dian Yuanda <yuanda.dian@gmail.com>
2018-06-11 13:32:01 +0700

    Add Login API and Creating JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://yuanda:yuanda26@ds255260.mlab.com:55260/proconnector'
+  mongoURI: 'mongodb://yuanda:yuanda26@ds255260.mlab.com:55260/proconnector',
+  secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 // Load Users Model
 const User = require('../../models/User');
@@ -44,6 +46,41 @@ router.post('/register', (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find User by Email
+  User.findOne({ email }).then(user => {
+    // Check for User
+    if(!user){
+      return res.status(404).json({email: 'User Not Found!'})
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if(isMatch){
+        // User Match
+        const payload = { 
+          id: user.id, 
+          name: user.name,
+          avatar: user.avatar
+        } // Create JWT Payload
+
+        // Sign Token
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({success: true, token: 'Bearer '+ token});
+        });
+      } else {
+        return res.status(400).json({password: 'Password or Email Doesnt Match'});
+      }
+    });
   });
 });
 
