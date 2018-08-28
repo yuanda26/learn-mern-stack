d4ace7d04ff5d1a7ec36c039babc06c65058d607
Dian Yuanda <yuanda.dian@gmail.com>
2018-08-28 21:55:58 +0700

    Add React Router DOM Dependencies and Add Basic Route on Landing Page

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="display-3">Login</h1>
+      </div>
+    );
+  }
+}
