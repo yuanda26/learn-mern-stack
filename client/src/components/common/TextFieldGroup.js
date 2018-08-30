dbb4cc066f841f9bdb69d278e66c094307905115
Dian Yuanda <yuanda.dian@gmail.com>
2018-08-30 21:46:38 +0700

    Create Text Field Group Component

---
+++ client/src/components/common/TextFieldGroup.js
@@ -0,0 +1,50 @@
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const TextFieldGroup = ({
+  name,
+  placeholder,
+  value,
+  label,
+  errors,
+  info,
+  type,
+  onChange,
+  disabled
+}) => {
+  return (
+    <div className="form-group">
+      <input
+        type={type}
+        className={classnames("form-control form-control-lg", {
+          "is-invalid": errors
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {info && <small className="form-text text-muted">{info}</small>}
+      {errors && <div className="invalid-feedback">{errors}</div>}
+    </div>
+  );
+};
+
+TextFieldGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  disabled: PropTypes.string
+};
+
+TextFieldGroup.defaultProps = {
+  type: "text"
+};
+
+export default TextFieldGroup;
