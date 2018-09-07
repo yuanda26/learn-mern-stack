f490ba7f9d188bf02292dc0128efcb0873588bca
Dian Yuanda <yuanda.dian@gmail.com>
2018-09-07 17:12:08 +0700

    Display Experience and Education to Dashboard and Add Delete Functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -73,9 +73,49 @@ export const addEducation = (eduData, history) => dispatch => {
     );
 };
 
+// Delete Experience
+export const deleteExperience = id => dispatch => {
+  if (window.confirm("Are you sure want to Delete this Experience?")) {
+    axios
+      .delete(`/api/profile/experience/${id}`)
+      .then(res =>
+        dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
+
+// Delete Education
+export const deleteEducation = id => dispatch => {
+  if (window.confirm("Are you sure want to Delete this Education?")) {
+    axios
+      .delete(`/api/profile/education/${id}`)
+      .then(res =>
+        dispatch({
+          type: GET_PROFILE,
+          payload: res.data
+        })
+      )
+      .catch(err =>
+        dispatch({
+          type: GET_ERRORS,
+          payload: err.response.data
+        })
+      );
+  }
+};
+
 // Delete Account & Profile
 export const deleteAccount = () => dispatch => {
-  if (window.confirm("Are you sure?")) {
+  if (window.confirm("Are you sure want to Delete Your Profile?")) {
     axios
       .delete("/api/profile")
       .then(res =>
