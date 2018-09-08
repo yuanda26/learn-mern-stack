b526fdddbe642b7877ac8fccebdfc78966857215
Dian Yuanda <yuanda.dian@gmail.com>
2018-09-08 22:21:35 +0700

    Add Post Feed

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import AddExperience from "./components/add-credentials/AddExperience";
 import AddEducation from "./components/add-credentials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 
 import "./App.css";
 
@@ -87,6 +88,9 @@ class App extends Component {
                   path="/add-education"
                   component={AddEducation}
                 />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/feed" component={Posts} />
               </Switch>
             </div>
             <Footer />
