bf9a828b185c85a409afc80e1d6bdb34970d7b96
Dian Yuanda <yuanda.dian@gmail.com>
2018-09-09 22:16:23 +0700

    Modified Keys Schema

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/devconnector",
-  secretOrKey: "secret"
-};
-
-// mLab MongoDB URI
-// mongoURI: 'mongodb://yuanda:yuanda26@ds255260.mlab.com:55260/proconnector',
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
