c1b3997acc5794573687ce3cdaee510ab507a1bd
Dian Yuanda <yuanda.dian@gmail.com>
2018-09-09 20:15:15 +0700

    Create Add Comment Form Component

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -3,6 +3,7 @@ import {
   ADD_POST,
   GET_ERRORS,
   GET_POSTS,
+  GET_POST,
   POST_LOADING,
   DELETE_POST
 } from "./types";
@@ -40,6 +41,25 @@ export const getPosts = () => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Get Specific Post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
         payload: null
       })
     );
@@ -83,6 +103,24 @@ export const unlikePost = id => dispatch => {
   axios
     .post(`/api/posts/unlike/${id}`)
     .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Add Comment to Post
+export const addComment = (postId, commentData) => dispatch => {
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -1,8 +1,9 @@
 import {
   ADD_POST,
   GET_POSTS,
-  POST_LOADING,
-  DELETE_POST
+  GET_POST,
+  DELETE_POST,
+  POST_LOADING
 } from "../actions/types";
 
 const initialState = {
@@ -24,17 +25,22 @@ export default function(state = initialState, action) {
         posts: action.payload,
         loading: false
       };
-    case DELETE_POST:
+    case GET_POST:
       return {
         ...state,
-        posts: state.posts.filter(post => post._id !== action.payload)
+        post: action.payload,
+        loading: false
       };
     case ADD_POST:
       return {
         ...state,
         posts: [action.payload, ...state.posts]
       };
-
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
     default:
       return state;
   }
